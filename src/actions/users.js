8a278c0f134af7a443b4cdb962ea45175e3d5c58
David van Oudheusden <doudheusden@novacollege.nl>
2025-02-04 11:49:20 +0100

    added user actions

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,8 +1,28 @@
-export const RECEIVE_USERS = "RECEIVE_USERS";
+export const RECIEVE_USERS = 'RECIEVE_USERS'
+export const SAVE_USER_QUESTION = 'SAVE_USER_QUESTION'
+export const SAVE_USER_ANSWER = 'SAVE_USER_ANSWER'
 
-export function receiveUsers(users) {
+
+export function recieveUsers(users) {
     return {
-        type: RECEIVE_USERS,
+        type: RECIEVE_USERS,
         users,
-    };
+    }
+}
+
+export function saveUserQuestion(user, qid) {
+    return {
+        type: SAVE_USER_QUESTION,
+        user,
+        qid,
+    }
+}
+
+export function saveUserAnswer(user, qid, answer) {
+    return {
+        type: SAVE_USER_ANSWER,
+        user,
+        qid,
+        answer
+    }
 }
