da0d704167cbdb054a3db02c4609e9f576af5d36
David van Oudheusden <doudheusden@novacollege.nl>
2025-02-11 16:10:20 +0100

    added tests to test data.js

--- src/__tests__/_DATA.test.js
+++ src/__tests__/_DATA.test.js
@@ -1,4 +1,4 @@
-import { _saveQuestion } from '../utils/_DATA';
+import { _saveQuestion, _saveQuestionAnswer } from '../utils/_DATA';
 
 describe('_saveQuestion', () => {
     it('should return saved question with all expected fields when correctly formatted data is passed', async () => {
@@ -21,5 +21,104 @@ describe('_saveQuestion', () => {
         expect(savedQuestion.optionOne.votes).toEqual([]);
         expect(savedQuestion.optionTwo.text).toBe('Learn Angular');
         expect(savedQuestion.optionTwo.votes).toEqual([]);
+    });
+
+    describe('should return an error if incorrect data is passed', () => {
+        it('when optionOneText is missing', async () => {
+            const invalidData = {
+                optionTwoText: 'Option Two',
+                author: 'sarahedo'
+            };
+
+            await expect(_saveQuestion(invalidData))
+                .rejects.toEqual('Please provide optionOneText, optionTwoText, and author');
+        });
+
+        it('when optionTwoText is missing', async () => {
+            const invalidData = {
+                optionOneText: 'Option One',
+                author: 'sarahedo'
+            };
+
+            await expect(_saveQuestion(invalidData))
+                .rejects.toEqual('Please provide optionOneText, optionTwoText, and author');
+        });
+
+        it('when author is missing', async () => {
+            const invalidData = {
+                optionOneText: 'Option One',
+                optionTwoText: 'Option Two'
+            };
+
+            await expect(_saveQuestion(invalidData))
+                .rejects.toEqual('Please provide optionOneText, optionTwoText, and author');
+        });
+
+        it('when all properties are missing', async () => {
+            const invalidData = {};
+
+            await expect(_saveQuestion(invalidData))
+                .rejects.toEqual('Please provide optionOneText, optionTwoText, and author');
+        });
+    });
+});
+
+describe('_saveQuestionAnswer', () => {
+    it('should save the answer and return true when correctly formatted data is passed', async () => {
+        // Arrange
+        const authedUser = 'sarahedo';
+        const qid = '8xf0y6ziyjabvozdd253nd';
+        const answer = 'optionOne';
+
+        const answerData = {
+            authedUser,
+            qid,
+            answer
+        };
+
+        // Act
+        const result = await _saveQuestionAnswer(answerData);
+
+        // Assert
+        expect(result).toBe(true);
+    });
+
+    describe('should return an error if incorrect data is passed', () => {
+        it('when authedUser is missing', async () => {
+            const invalidData = {
+                qid: '8xf0y6ziyjabvozdd253nd',
+                answer: 'optionOne'
+            };
+
+            await expect(_saveQuestionAnswer(invalidData))
+                .rejects.toEqual('Please provide authedUser, qid, and answer');
+        });
+
+        it('when qid is missing', async () => {
+            const invalidData = {
+                authedUser: 'sarahedo',
+                answer: 'optionOne'
+            };
+
+            await expect(_saveQuestionAnswer(invalidData))
+                .rejects.toEqual('Please provide authedUser, qid, and answer');
+        });
+
+        it('when answer is missing', async () => {
+            const invalidData = {
+                authedUser: 'sarahedo',
+                qid: '8xf0y6ziyjabvozdd253nd'
+            };
+
+            await expect(_saveQuestionAnswer(invalidData))
+                .rejects.toEqual('Please provide authedUser, qid, and answer');
+        });
+
+        it('when all properties are missing', async () => {
+            const invalidData = {};
+
+            await expect(_saveQuestionAnswer(invalidData))
+                .rejects.toEqual('Please provide authedUser, qid, and answer');
+        });
     });
 }); 
